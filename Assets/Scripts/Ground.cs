29dda559f7202424ed9c99f7e6a4d58ce9965460
Vladislav <GimliVlad@list.ru>
2023-11-22 17:19:21 +0300

    replace List to parent object

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -3,21 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ground : MonoBehaviour
-{
-    private List<Resource> _resources = new List<Resource>();
-    private int _firstIndexNumber = 0;
-
-    public bool HasResource => _resources.Count > 0;
+{    
+    public bool HasResource => transform.childCount > 0;    
 
     public void AddResource(Resource resource)
     {
-        _resources.Add(resource);
-    }
-
-    public Resource GetResource()
-    {
-        Resource resource = _resources[_firstIndexNumber];       
-        _resources.Remove(resource);
-        return resource;
+        resource.name = NamesData.Resource;
+        resource.transform.SetParent(transform);
     }
 }
